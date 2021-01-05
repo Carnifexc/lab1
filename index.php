32a6e411155e36d4dab25f85f14de9776f6e67ba
Виктория <calipesto@inbox.ru>
2021-01-05 16:54:21 +0300

    Первая лаба

---
+++ index.php
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE htm1>
+<html>
+	<head>
+		<title> Моя страничка </title>
+		<link rel="stylesheet" href="mycss.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "<h1>Основание треугольника 6, высота 4. Площадь этого треугольника равняется:", (6*4)/2;"</h1>"
+			?>
+		</div>
+	</body>
+</html>
